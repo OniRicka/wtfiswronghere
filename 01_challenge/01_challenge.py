4e6e4ce46af130142e2bf85d4d30a6426d004613
OniRicka <aleksandrmajorcik36@gmail.com>
2026-08-10 02:08:46 +0300

    Update 01_challenge.py

--- 01_challenge/01_challenge.py
+++ 01_challenge/01_challenge.py
@@ -1,13 +1,13 @@
-"""
-We will use this script to teach Python to absolute beginners
-The script is an example of Fizz-Buzz implemented in Python
+#"""
+#We will use this script to teach Python to absolute beginners
+#The script is an example of Fizz-Buzz implemented in Python
 
-The FizzBuzz problem: 
-For all integers between 1 and 99 (include both):
+#The FizzBuzz problem: 
+#For all integers between 1 and 99 (include both):
     # print fizz for multiples of 3
     # print buzz for multiples of 5 
     # print fizzbuzz for multiples of 3 and 5"
-"""
+#"""
 
 def fizzbuzz(max_num):
     "This method implements FizzBuzz"
@@ -22,12 +22,12 @@ def fizzbuzz(max_num):
     # Google for 'range in python' to see what it does
     for i in range(1,max_num):
         # % or modulo division gives you the remainder 
-        if i%num1==0 and i%num2==0:
+        if i%num1== 0 and i%num2== 0;
             print(i,three_mul+five_mul)
-        elif i%num1=0:
+        elif i%num1== 0;
             print(i,three_mul)
-        elif i%num2==0:
-            print(i,five_mul)
+        elif i%num2== 0;
+            print(i, five_mul)
 
 #----START OF SCRIPT
 if __name__=='__main__':
